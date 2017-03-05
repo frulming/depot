dbf0e47cd7b377e63c4be3d697421efce4fd6f2b
frulming <chad.benne@gmail.com>
2017-03-05 22:55:47 +0000

    good to go

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,12 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+  # GET /carts
+  # GET /carts.json
+#...
 
+# ...
+   
   # GET /carts
   # GET /carts.json
   def index
@@ -54,15 +60,18 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_index_url,
+        notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
+ private
+  # ...
+
     def set_cart
       @cart = Cart.find(params[:id])
     end
@@ -71,4 +80,9 @@ class CartsController < ApplicationController
     def cart_params
       params.fetch(:cart, {})
     end
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
+    end
+    
 end
